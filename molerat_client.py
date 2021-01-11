6bd253722b4ecba21e428af1cf79061b391165b0
Szymon Majkut <Szymek_diks@interia.pl>
2021-01-11 02:42:17 +0100

    Scheme of communication added

--- molerat_client.py
+++ molerat_client.py
@@ -1,10 +1,24 @@
 import socket
 
 
+print("Welcome in molerats!")
 addr = ('localhost', 65420)
+print("We will try to connect to server at host ", addr[0], " on port ", addr[1])
+print("First send your nick, ,,start'' will finish the lobby and start the game on server")
+print("After each iteration you will be informed about game status")
+print("Move using wasd keys, 1,2 and 3 will allow you to make noise, use r to pick a food")
+print("Remember! It is easy to get lost in molerats tunnels...")
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect(addr)
+	
+    msg = input("I am ")
+    s.sendall(msg.encode())
+    data = s.recv(1024)
+    print('Welcome!', repr(data))
+	
     while True:
-        s.sendall(b'Hello, world')
+        msg = input("To server << ")
+        s.sendall(msg.encode())
         data = s.recv(1024)
         print('Received', repr(data))
--- molerat_server.py
+++ molerat_server.py
@@ -1,29 +1,98 @@
 import socket
 import _thread
+import time
 
 
 service_address = ('', 65420)
 
-def on_new_client(clientsocket,addr):
-    print("du[a")
-    with clientsocket as s:
-        print("du[a")
-        while True:
-            msg = s.recv(1024)
-            #do some checks and if msg == someWeirdSignal: break:
-            print(addr, ' >> ', msg)
-            msg = b'SERVER >> reponse'
-        
-            print("Game proceeding...")
-        
-            s.send(msg)
+class Command:
+    def __init__(self):
+        print("New command from player!")
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    print('Server started!')
-    print('Waiting for clients...')
-    s.bind(service_address)
-    s.listen()
+class NoopCommand:
+    pass
+
+class Player:
+    def __init__(self, nick):
+        self.x_position = 0
+        self.y_position = 0
+        self.nick = nick
+        self.current_command = NoopCommand()
+
+    def move(self, x_vector, y_vector):
+        self.x = self.x + x_vector
+        self.y = self.y + y_vector
+
+    def execute_command(self, game):
+        print("Here happens changes due to the last " + str(self.nick) + " command")
+        self.current_command = NoopCommand()
+
+class Game:
+    def __init__(self):
+        self.running = False
+        self.round = 0
+        self.players = dict()
+
+    def add_player(self, addr, player):
+        self.players[addr] = player
+
+    def render_for_player(self, addr):
+        render = "Position: (" + str(self.players[addr].x_position) + "),(" + str(self.players[addr].y_position) + ")"
+        render = render + "\n other info..."
+        return render
+
+def new_client(conn, addr, game):
+    with conn as client_socket:
+        nick = client_socket.recv(1024).decode()
+        player = Player(nick)
+        game.add_player(addr, player)
+        client_socket.send(nick.encode())
+
+        while True:
+            msg = client_socket.recv(1024)
+
+            if msg.decode() == "start":
+                msg = "Game started by " + str(player.nick)
+                game.running = True
+
+            if game.running:
+                msg = b'Game starts in seconds...'
+                client_socket.send(msg)
+                break
+
+            msg = b'Server waits for command to start...'
+            client_socket.send(msg)
+
+        while True:
+            msg = client_socket.recv(1024)
+            player.command = Command()
+            rendered_game = game.render_for_player(addr)
+            client_socket.send(rendered_game.encode())
+
+def game_loop(game):
     while True:
-        conn, addr = s.accept()
-        print('Connected by', addr)
-        _thread.start_new_thread(on_new_client,(conn,addr))
+        if (game.running):
+            for player in game.players.values():
+                player.execute_command(game)
+            print('Game updated!')
+            game.round = game.round + 1
+            time.sleep(5)
+        else:
+            print('Lobby:')
+            for p in game.players.values():
+                print(p.nick)
+            time.sleep(10)
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+    print('Server started!')
+    game = Game()
+    _thread.start_new_thread(game_loop,(game,))
+    print('Waiting for clients...')
+    server_socket.bind(service_address)
+    server_socket.listen(5)
+
+    while True:
+        conn, addr = server_socket.accept()
+        print('New connection! ', addr)
+        _thread.start_new_thread(new_client,(conn, addr,game))
